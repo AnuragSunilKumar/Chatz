ac14f827e7b37f43ae35f280f94784d7c14c0a98
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-06-03 20:42:41 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+
 /* eslint-disable import/no-unresolved */
 
 import React from 'react'
-
+import { Switch } from 'react-router';
+import PrivateRoute from './Components/PrivateRoute'
 import 'rsuite/dist/styles/rsuite-default.css';
+import Signin from './Pages/Signin';
+import Home from './Pages/Home';
 import './styles/main.scss';
+import PublicRoute from './Components/PublicRoute';
 
 function App() {
   return (
-    <div>Hello</div>
+    <Switch>
+      <PublicRoute path ="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRoute path ="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
 
   );
 }
