3452d106563f5c15fde8f6452ead330bf6c664d0
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-06-04 20:29:31 +0530

    started the sidebar component

---
+++ src/Components/dashBoard/dashboardToggle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button, Drawer, Icon } from 'rsuite'
+import DashBoard from '.'
+import { useModalState } from '../../misc/custom-hooks'
+
+const DashboardToggle = () => {
+
+ const {isOpen ,close , open} = useModalState()
+
+
+  return (
+    <>
+     <Button block color="blue" onClick={open}>
+       <Icon icon="dashboard"/> DashBoard
+       </Button> 
+       <Drawer show={isOpen} onHide={close} placement="left">
+         <DashBoard />
+
+       </Drawer>
+    </>
+  )
+}
+
+export default DashboardToggle
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Col, Grid, Row } from 'rsuite'
+import Sidebar from '../Components/Sidebar';
 
 const Home = () => {
   return (
-    <div>
-      Hello Home
-    </div>
+    <Grid fluid className ="h-100">
+      <Row>
+        <Col xs = {24} md={8}>
+          <Sidebar />
+
+        </Col>
+      </Row>
+
+    </Grid>
   )
 }
 
