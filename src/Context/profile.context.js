426662dd4c2a1040a3d5c9aef96d8a2c21ca42ed
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-06-04 18:53:01 +0530

    manage user with profile context;

--- src/Context/profile.context.js
+++ src/Context/profile.context.js
@@ -1,12 +1,61 @@
-import React, { createContext, useState, useContext } from "react";
-
+import React, { createContext, useState, useContext, useEffect } from "react";
+import {auth, database} from '../misc/Firebase' 
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({children}) => {
 
-  const [profile] =useState(false);
+  const [profile, setProfile] =useState(null);
+  const [isLoading, setisLoading] = useState(true);
 
-  return( <ProfileContext.Provider value = {profile}>{children}</ProfileContext.Provider>);
+
+  useEffect(()=>{
+
+    let userRef;
+    
+  
+    const authUnsub = auth.onAuthStateChanged(authObj =>{
+     
+      if(authObj){
+
+        userRef = database.ref(`/profiles/${authObj.uid}`)
+        userRef.on('value',snap =>{
+         
+          const {name , createdAt} = snap.val();
+
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email
+          };
+          setProfile(data);
+          setisLoading(false);
+
+        });
+
+      }else{
+
+        if(userRef)
+        {
+          userRef.off();
+        }
+        setProfile(null);
+        setisLoading(false);
+
+      }
+    })
+
+    return () =>{
+      authUnsub();
+
+      if (userRef){
+        userRef.off();
+      }
+    }
+
+  },[])
+
+  return( <ProfileContext.Provider value = {{isLoading,profile}}>{children}</ProfileContext.Provider>);
 };
 export const useProfile = () => useContext(ProfileContext);
