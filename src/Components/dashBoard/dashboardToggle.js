a913b1b8bf3ab2329a292ef2b98f1b959e54870b
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-06-05 12:59:52 +0530

    made a responsive sidebar

--- src/Components/dashBoard/dashboardToggle.js
+++ src/Components/dashBoard/dashboardToggle.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Button, Drawer, Icon } from 'rsuite'
 import DashBoard from '.'
-import { useModalState } from '../../misc/custom-hooks'
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
 
 const DashboardToggle = () => {
 
- const {isOpen ,close , open} = useModalState()
+ const {isOpen ,close , open} = useModalState();
+ const isMobile = useMediaQuery('max-width: 998px');
 
 
   return (
@@ -13,7 +14,7 @@ const DashboardToggle = () => {
      <Button block color="blue" onClick={open}>
        <Icon icon="dashboard"/> DashBoard
        </Button> 
-       <Drawer show={isOpen} onHide={close} placement="left">
+       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
          <DashBoard />
 
        </Drawer>
