a18b2ff6674dbb36f3a8a38f478e644e641c0690
AnuragSunilKumar <anuragsunilkumar1005@gmail.com>
2021-06-06 11:24:28 +0530

    created button to create new chat room

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import DashboardToggle from './dashBoard/dashboardToggle'
+import React from 'react';
+import CreateRoomBtnModal from './CreateRoomBtnModal';
+import DashboardToggle from './dashBoard/dashboardToggle';
 
 const Sidebar = () => {
   return (
     <div className="h-100 pt-2">
-
       <div>
         <DashboardToggle />
+        <CreateRoomBtnModal />
       </div>
-      
       bottom
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
